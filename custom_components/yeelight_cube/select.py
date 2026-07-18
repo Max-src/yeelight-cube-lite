dd3dceb6b1d49980e9e7078b6a87444f4b3c2d0f
Kevin6872 <Kevin6872@users.noreply.github.com>
2026-07-18 12:15:27 +0800

    Group clock configuration entities

--- custom_components/yeelight_cube/select.py
+++ custom_components/yeelight_cube/select.py
@@ -640,6 +640,7 @@ _CLOCK_STYLE_TO_ID = {
 class YeelightCubeClockStyleSelect(SelectEntity):
     """Select a native firmware clock face."""
 
+    _attr_entity_category = EntityCategory.CONFIG
     _attr_has_entity_name = True
     _attr_should_poll = False
     _attr_translation_key = "clock_style"
